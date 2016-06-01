a12ea400d312df32c7d71f50d7df70e71414fa07
Alvin Varghese <alvin@nfnlabs.in>
2016-06-01 17:22:17 +0530

    Initial Commit

---
+++ MyUtilityClassesTests/MyUtilityClassesTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyUtilityClassesTests.swift
+//  MyUtilityClassesTests
+//
+//  Created by Alvin Varghese on 6/1/16.
+//  Copyright © 2016 Swift Coder. All rights reserved.
+//
+
+import XCTest
+@testable import MyUtilityClasses
+
+class MyUtilityClassesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
